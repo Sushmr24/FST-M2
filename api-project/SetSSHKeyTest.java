390455ba0aa106f5914d97478a668d1c02b4f026
Sushmr24 <97938738+Sushmr24@users.noreply.github.com>
2022-05-25 12:41:56 +0530

    Add files via upload

--- api-project/SetSSHKeyTest.java
+++ api-project/SetSSHKeyTest.java
@@ -15,14 +15,14 @@ import static io.restassured.RestAssured.given;
 public class SetSSHKeyTest {
     // Set base URL
     final static String baseURI = "https://api.github.com";
-    static String SSHKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFWMfyWQRuPnaa24ATt9DwnXyi2aD405PDJS0OUD0uKoPojTVsxRyb0VygIoIWgRXSzMZlEasQNiAhb8FL6wXLjIqWODYHUQr5KMeEYiy55X1DhkgZw3MQmvEM/2Y8IBTXwq6zn9iZW0jaVX/0c/l8W068QkLAG6Jm0tyniYP/w9oHQs4BSjUzVUEH3XC8CX+CTVfgTUUy+e7pNTE04E3DUfCqAMJn9D9rIcLibnlia2Dg5URK7iRcXRPrdBTe/+x7yBvH5K/4Nn9CC4eqKKDhZ7Ee+40rYRUqfVZncgCYwM2kOJ3a051C+D6sJoZop+ohxRHUJDdgDuyck/SC8+aadt2YVMJMB3L+iUdN+ScQ51QvrnHpJCXZeATHBAxFUcfDTChsfder4XCJqpMwsYEFx6JaKowFlbi3Cx2F/uRUpgOAlT+oR0ZQfsvtQSmQ/6tq6kTLoD8Mv9/9XUyouZoFcSJgNURtJTCwonzf+kaMWd6PPjEKR1/uESYzXkrrtG8=";
+    static String SSHKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFWMfyWQRuPnaa24ATt9DwnXyi2XXXXXXXXXXX";
     static int id ;
     RequestSpecification requestSpec ;
 
     @BeforeClass
     public void setup()
     {
-        requestSpec =  new RequestSpecBuilder().setContentType(ContentType.JSON).addHeader("Authorization","token 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").setBaseUri(baseURI).build();
+        requestSpec =  new RequestSpecBuilder().setContentType(ContentType.JSON).addHeader("Authorization","token ghp_RW2Xgxxxxxxxxxxxxxxx").setBaseUri(baseURI).build();
     }
 
 
